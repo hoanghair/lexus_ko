70235641582764e20d0e2f8ffe3c2d5205e9ebd9
hoanghair <96115510+hoanghair@users.noreply.github.com>
2023-07-21 00:11:17 +0700

    update

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-$(document).ready(function() {
-  $(".list__menu-item").hover(
-    function() {
-      $(".list__menu-item").not(this).css("opacity", "0.4");
-    },
-    function() {
-      $(".list__menu-item").css("opacity", "1");
-    }
-  );
+$(document).ready(function () {
+  function onView(e) {
+    e.preventDefault();
+    const targetIdNav = $(this).data("id");
+    console.log(targetIdNav);
+
+    $(".item__child").removeClass("is_show").addClass("off");
+
+    $(".item__child").each(function () {
+      const targetIdChild = $(this).attr("id");
+      console.log(targetIdChild);
+      if (targetIdNav === targetIdChild) {
+        $(this).addClass("is_show").removeClass("off");
+        $("body").css("overflow", "hidden");
+      }
+      if ($(".item__child.is_show")) {
+        $("list__item-child").css("transform", "translateX(0)");
+      }
+    });
+  }
+
+  $(".header__menu-item").hover(onView);
+
+  // $(".header__menu-item").mouseleave(function () {
+  //   $(".item__child").removeClass("is_show").addClass("off");
+  //   $("body").css("overflow", "auto");
+  // });
 });
