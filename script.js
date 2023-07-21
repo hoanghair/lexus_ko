8c5cdfafb0e09ec77db8831ba1001090d36ec660
hainh <hoanghai16072000@gmail.com>
2023-07-21 17:04:20 +0700

    upadate

--- script.js
+++ script.js
@@ -13,13 +13,40 @@ $(document).ready(function () {
         $(this).addClass("is_show").removeClass("off");
         $("body").css("overflow", "hidden");
       }
-      if ($(".item__child.is_show")) {
-        $("list__item-child").css("transform", "translateX(0)");
+      if ($(".list__item-child .is_show")) {
+        $(".list__item-child").css("transform", "translateX(0)");
+      }else if($(".list__item-child .is_show")){
+        $(".list__item-child").css("transform", "translateX(-100%)");
+      }
+    });
+  }
+  function onViewLvTwo(e) {
+    e.preventDefault();
+    const targetIdNav = $(this).data("id");
+    const childLvTwo = $('.child__lvtwo')
+    const itemChildLvTwo = $('.child__lvtwo-item')
+    console.log(targetIdNav);
+
+    $(itemChildLvTwo).removeClass("is_show").addClass("off");
+
+    $(itemChildLvTwo).each(function () {
+      const targetIdChild = $(this).attr("id");
+      console.log(targetIdChild);
+      if (targetIdNav === targetIdChild) {
+        $(this).addClass("is_show").removeClass("off");
+        $("body").css("overflow", "hidden");
+      }
+      if ($(".child__lvtwo .is_show")) {
+        $(".child__lvtwo").css("transform", "translateX(0)");
+      }else if($(".child__lvtwo .is_show")){
+        $(".child__lvtwo").css("transform", "translateX(-100%)");
       }
     });
   }
 
   $(".header__menu-item").hover(onView);
+  $(".item__child-list li").hover(onViewLvTwo);
+
 
   // $(".header__menu-item").mouseleave(function () {
   //   $(".item__child").removeClass("is_show").addClass("off");
